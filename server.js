fbe407cc6496796d41f1203f6752f2379632d983
vorSherer <tcpma2011@gmail.com>
2020-03-17 19:28:54 -0700

    Updated server code (incomplete code commented out).

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 'use strict';
 
 //libraries
+// gets the variables from our hiding place
+require('dotenv').config();
 
 // my server
 const express = require('express');
 const app = express();
+// const superagent = require('superagent');
 
-// gets the variables from our hiding place
-require('dotenv').config();
+// const cors = require('cors');
+// const express = require('express');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(cors());
+// app.use(express.static('./public'));
 
 // the underpaid security guard
 const cors = require('cors');
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+
+// superagent.get('url')
+//     .then(results => { do something, e.g. console.log('PoL') })
+//     .catch(err => console.error(err))
+
+// $.ajax('url', {method: "GET", datatype: "JSON"})
+//  .then 
 
 app.get('/location', (request, response) => {
   // this is the city that the front end is sending us in the qurey
@@ -26,9 +42,30 @@ app.get('/location', (request, response) => {
     response.send(location);
   }
   catch(err){
+    response.status(500).send(err);
     console.error(err);
   }
 })
+
+// NEW Live Location Route - under construction:
+// app.get('/location', (request, response) => {
+//   // this is the city that the front end is sending us in the qurey
+//   // the query lives in the url after the ? htt://cooldomain.com?city=seattle
+//   try{
+//     let city = request.query.city;
+//     // let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.YOUR-API-KEY}&q=${city}&format=json`;
+//     // superagent.get(url)
+
+//     // let geo = require('./data/geo.json');
+//     // let location = new Location(geo[0], city)
+//     // response.send(location);
+//   }
+//   catch(err){
+//     response.status(500).send(err);
+//     console.error(err);
+//   }
+// })
+
 
 function Location(obj, city){
   this.search_query = city;
@@ -37,8 +74,40 @@ function Location(obj, city){
   this.longitude = obj.lon;
 }
 
+// **************
+// app.get('/weather', (request, response) => {
+//     // figure out what the front end sent
+//     console.log('From the front end: ', request.query);
+//     // let city = request.query.search_query;
+//     // let formatted_query = request.query.formatted_query;
+//     // let latitude = request.query.latitude;
+//     // let longitude = request.query.longitude;
+
+//     // get data from the darksky file
+//     // try{
+//         let weather = require('./data/darksky.json');
+//         let weatherArray = weather.daily.data;
+//         // let finalWeatherArray = weatherArray.map(day => {
+//         //     return new Weather(weather.daily.data[0])       })
+        
+
+//         // response.send(forecast);
+//     }
+//     // catch(err){
+//     //     console.error(err);
+//     }
+// })
+// // the query lives in the url after the ? htt://cooldomain.com?city=seattle
+  
+//   function Weather(obj){
+//     this.time = new Date(obj.time * 1000).toDateString();
+//     this.forecast = obj.summary;
+//   }
+  
+// ***************
+
 app.get('*', (request, response) => {
-  response.status(404).send('there is nothing on this page');
+  response.status(404).send('Sorry, that route dos not exist.');
 })
 
 // turn on the server
@@ -47,15 +116,6 @@ app.listen(PORT, () => {
 })
 
 
-// require('dotenv').config();
-// const cors = require('cors');
-// const express = require('express');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(cors());
-// app.use(express.static('./public'));
 
 // app.get('/', (req,res) => {
 //     res.status(200).send('Hello!')
