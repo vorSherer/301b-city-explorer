10263e6bb261ae299216f7e78d9e71e46b7f07c9
vorSherer <tcpma2011@gmail.com>
2020-03-21 21:41:03 -0700

    Preparing server.js for modularization.

--- server.js
+++ server.js
@@ -35,27 +35,51 @@ client.connect()
 
 
 // endpoint calls
-app.get('/location', (request, response) => {
-    const city = request.query.city;
-    let sql = 'SELECT * FROM locations WHERE search_query = $1;';
-    let safeValues = [city];
-    client.query(sql, safeValues)
-    .then(sqlCheck => {
-      if(sqlCheck.rows.length > 0) {
-        response.status(200).send(sqlCheck.rows[0]);
-      } else {
-        const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
-        superagent.get(url)
-        .then(results => {
-          const location = new Location(results.body[0], city)
-          sql = 'INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1, $2, $3, $4);';
-          safeValues = [location.search_query, location.search_query, location.latitude, location.longitude];
-          client.query(sql,safeValues);
-          response.status(200).send(location);
-        }).catch(err => errorHandler(err, response));
-      }
-    })
-});
+app.get('/location', locationHandler);
+
+function locationHandler (request, response) => {
+  const city = request.query.city;
+  let sql = 'SELECT * FROM locations WHERE search_query = $1;';
+  let safeValues = [city];
+  client.query(sql, safeValues)
+  .then(sqlCheck => {
+    if(sqlCheck.rows.length > 0) {
+      response.status(200).send(sqlCheck.rows[0]);
+    } else {
+      const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
+      superagent.get(url)
+      .then(results => {
+        const location = new Location(results.body[0], city)
+        sql = 'INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1, $2, $3, $4);';
+        safeValues = [location.search_query, location.search_query, location.latitude, location.longitude];
+        client.query(sql,safeValues);
+        response.status(200).send(location);
+      }).catch(err => errorHandler(err, response));
+    }
+  })
+};
+
+// app.get('/location', (request, response) => {
+//     const city = request.query.city;
+//     let sql = 'SELECT * FROM locations WHERE search_query = $1;';
+//     let safeValues = [city];
+//     client.query(sql, safeValues)
+//     .then(sqlCheck => {
+//       if(sqlCheck.rows.length > 0) {
+//         response.status(200).send(sqlCheck.rows[0]);
+//       } else {
+//         const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
+//         superagent.get(url)
+//         .then(results => {
+//           const location = new Location(results.body[0], city)
+//           sql = 'INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1, $2, $3, $4);';
+//           safeValues = [location.search_query, location.search_query, location.latitude, location.longitude];
+//           client.query(sql,safeValues);
+//           response.status(200).send(location);
+//         }).catch(err => errorHandler(err, response));
+//       }
+//     })
+// });
 
 function Location(obj, city) {
   this.search_query = city;
